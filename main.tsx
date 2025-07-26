95be8f0d7285ea2b2258191f37265acb0a2f4181
Ezra Natanael <ezrantn@proton.me>
2025-07-26 18:53:00 +0700

    update

--- main.tsx
+++ main.tsx
@@ -3,12 +3,14 @@ import blog, { ga } from "blog";
 blog({
   title: "Ezra Natanael",
   author: "Ezra Natanael",
-  avatar: "./posts/static/pic-of-me.jpeg",
+  avatar: "./posts/static/_.jpeg",
   avatarClass: "rounded-full",
+  description: "Software Engineer",
   links: [
     { title: "Email", url: "mailto:ezrantn@proton.me" },
     { title: "GitHub", url: "https://github.com/ezrantn" },
     { title: "LinkedIn", url: "https://www.linkedin.com/in/ezrantn" },
+    { title: "Google Scholar", url: "https://scholar.google.com/citations?hl=en&user=iubOEX4AAAAJ" }
   ],
   middlewares: [
     ga("G-RJ3SV8DLML"),
